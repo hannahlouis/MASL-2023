df74c01773bfca872e1b7ddb6f93f9892a269d15
hannahlouis <hlouis@smu.edu>
2023-10-03 01:17:20 -0500

    completed findPeaks()

--- AudioLabSwift-master 2/AudioLabSwift/AudioModel.swift
+++ AudioLabSwift-master 2/AudioLabSwift/AudioModel.swift
@@ -28,6 +28,7 @@ class AudioModel {
     var max1:[Float]
     var max2:[Float]
     
+    
     // MARK: Public Methods
     init(buffer_size:Int) {
         BUFFER_SIZE = buffer_size
@@ -159,6 +160,10 @@ class AudioModel {
             
             twentyPointData = getTwentyPointData()
             
+//            findPeaks1(arr: fftData)
+            let x = findPeaks2()
+            
+            print (convert(k:x.0), "," , convert(k:x.1))
             
         }
     }
@@ -252,4 +257,106 @@ class AudioModel {
         twentyData.append(max2Data)
         return twentyData // returns 20 float array of max values in sections
     }
+    //use get twenty point data or not? this gives the maxes so maybe i could like use a find function to find where these are in the og ?
+    
+    //given an array, if there is a peak, find and return the index ->
+    // peak is where the values on either side are less than
+    // do i need to compare more than just the values before and after
+    
+    func findPeaks1(arr:[Float]) -> (Float,Float){
+        var absMax = Float(-10000.0);
+        var relMax = Float(-100000.0);
+        
+        var p = 0;
+        var x = 1;
+        var n = 2;
+        for i in arr { // is i the index or the value?
+            if p == 0{
+                var next = arr[n]
+                if (i>next){
+                    absMax = i
+                }
+                
+            }else {
+                var next = arr[n]
+                var prev = arr[p]
+                if (i > prev && i > next){
+                    relMax = absMax
+                    absMax = i
+                }
+                p+=1
+                n+=1
+            }
+        }
+         print (absMax, ",", relMax)
+        return (absMax,relMax)
+    }
+    
+    func findPeaks2()-> (Int,Int){
+        var max1 = Float(-1000000)
+        var max2 = Float(-10000000)
+        var index1 = 0
+        var index2 = 0
+        //var max = -10000;
+        for i in 0...BUFFER_SIZE/2{
+//        for i in fftData{
+            
+            if (i+17 <  BUFFER_SIZE/2){
+                var window = fftData[i...(i+16)]
+                var count = 1;
+                for j in window{
+                    count+=1
+                    if (j == window.max() && count == 9){
+                        if (j > max1 && j > max2){
+                            max2 = max1
+                            max1 = j
+                            index2 = index1
+                            index1 = i + 9
+                        } else if ( j > max2 && j < max1 ){
+                            max2 = j
+                            index2 = i + 9
+                            
+                        }
+                    }
+                    
+                    
+                    
+//                    if (j > max){
+//                         max = window[j]
+//                    }
+//                    if j == 5{
+//                        max2 = max1
+//                        max1 = max
+//                    }
+                    
+                }
+            }
+            
+//            var wind = fftData[i]
+           
+        
+            
+            
+            
+            
+//            for j in window{
+//                if max < window[j]{
+//                    max = window[j]
+//                }
+//                if max = window[5]{
+//                    globalMax = window[5]
+//                }
+//            }
+        }
+        
+//        print (max1 , "," , max2)
+        return (index1,index2)
+    }
+    
+    func convert (k:Int) -> (Double){
+        let freq = (Double(k)*(audioManager!.samplingRate))/Double(BUFFER_SIZE)
+        return freq
+            
+    }
+    
 }
